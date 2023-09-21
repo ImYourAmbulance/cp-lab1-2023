d375dfc7a239ac68e81054487fd2b89ed99e42e3
Iliia Denisov <iliiadenisov@Iliias-MacBook-Air.local>
2023-09-21 16:50:33 +0300

    add some basic tests

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,18 @@
 class Cache{
+    Get(key) {
+        if (this.#hits.has(key) && this.#hits.get(key) > 0) {
+            this.#hits.set(key, this.#hits.get(key) - 1);
+            return this.#values.get(key);
+        }
+        return null;
+    }
 
+    Add(key, value, hits = 1) {
+        this.#hits.set(key, hits);
+        this.#values.set(key, value);
+    }
+
+    #values = new Map();
+    #hits = new Map();
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,17 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+test('Base case', () => {
+    const cache = new Cache();
+    cache.Add("key", "value", 1);
+
+    expect(cache.Get("key")).toBe("value");
+});
+
+
+test('Should decrease element counter', () => {
+    const cache = new Cache();
+    cache.Add("key", "value", 1);
+    cache.Get("key");
+
+    expect(cache.Get("key")).toBe(null);
 });
